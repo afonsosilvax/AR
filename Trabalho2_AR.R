85798a5b49302019153f110f29ae8821d5ed906d
Afonso Gião <99890880+afonsosilvax@users.noreply.github.com>
2023-12-29 17:28:37 +0000

    comunidades feito

--- Trabalho2_AR.R
+++ Trabalho2_AR.R
@@ -35,6 +35,7 @@ pa <- function(clique_dim, rede_dim, p, lig_iter){
   return(grafo)
 }
 
+set.seed(1)
 grafos <- lapply(1:10, function(x) pa(10, 200, 0.8, 3))
 
 h <- c()
@@ -55,15 +56,18 @@ for (i in 1:10) {
   d_med <- c(d_med, mean_distance(grafo, directed=F))
 }
 
+set.seed(1)
+
 plot(grafo)
-boxplot(h)
-boxplot(c_clust)
-boxplot(d_med)
+boxplot(h, main = "Parâmetro de heterogeneidade")
+boxplot(c_clust, main = "Coeficiente de clustering")
+boxplot(d_med, main = "Distância média")
   
 h
 c_clust
 d_med
 
+set.seed(1)
 grafos <- lapply(1:10, function(x) pa(20, 200, 0.8, 3))
 
 h <- c()
@@ -85,10 +89,12 @@ for (i in 1:10) {
   d_med <- c(d_med, mean_distance(grafo,directed=F))
 }
 
+set.seed(1)
+
 plot(grafo)
-boxplot(h)
-boxplot(c_clust)
-boxplot(d_med)
+boxplot(h, main = "Parâmetro de heterogeneidade")
+boxplot(c_clust, main = "Coeficiente de clustering")
+boxplot(d_med, main = "Distância média")
   
 h
 c_clust
@@ -96,14 +102,57 @@ d_med
 
 # Rede do trabalho 1
 grafo <- read.graph(here("trab_links.txt"), format = c('edgelist'), directed = F)
+
+set.seed(1)
 plot(grafo)
 
 # Estudo da componente gigante
 components<-components(grafo)
 comp_gig <- induced.subgraph(grafo, which(components$membership == which.max(components$csize)))
+
+set.seed(1)
 plot(comp_gig)
 
-# Métodos de deteção de comunidades
+## Métodos de deteção de comunidades
 
 # Método de Louvain
+set.seed(1)
 louvain <- cluster_louvain(comp_gig)
+
+# Número e dimensão das comunidades obtidas
+sizes(louvain)
+
+set.seed(1)
+plot(louvain, comp_gig)
+
+
+# Método de Edge Betweenness
+set.seed(1)
+ceb <- cluster_edge_betweenness(comp_gig)
+
+# Número e dimensão das comunidades obtidas
+sizes(ceb)
+
+set.seed(1)
+plot(ceb, comp_gig)
+
+# Método de Fast Greedy
+set.seed(1)
+clm <- cluster_fast_greedy(comp_gig)
+
+# Número e dimensão das comunidades obtidas
+sizes(clm)
+
+set.seed(1)
+plot(clm, comp_gig)
+
+
+# Metodo Label Propagation
+set.seed(1)
+clp <- cluster_label_prop(comp_gig)
+
+# Número e dimensão das comunidades obtidas
+sizes(clp)
+
+set.seed(1)
+plot(clp, comp_gig)
